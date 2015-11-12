46b523b70a0dbfaf4d23169099e33ff63138f42f
MIN LI <minli@us.ibm.com>
2015-11-12 11:42:34 -0800

    minor change

--- mllib/src/main/scala/org/apache/spark/mllib/linalg/distributed/BlockMatrix.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/linalg/distributed/BlockMatrix.scala
@@ -352,40 +352,31 @@ class BlockMatrix @Since("1.3.0") (
     }
   }
 //lmlimin-begin
-      def elementAdd(m1:BlockMatrix,eps : Double): BlockMatrix = {
-        require(m1.numRows() == m2.numRows(), "Both matrices must have the same number of rows. " +
-            s"A.numRows: ${numRows()}, B.numRows: ${m2.numRows()}")
-        require(m1.numCols() == m2.numCols(), "Both matrices must have the same number of columns. " +
-            s"A.numCols: ${m1.numCols()}, B.numCols: ${m2.numCols()}")
-        if (m1.rowsPerBlock == m2.rowsPerBlock && m1.colsPerBlock == m2.colsPerBlock) {
-            val addedBlocks = m1.blocks.cogroup(m2.blocks, m1.createPartitioner())
-                .map { case ((blockRowIndex, blockColIndex), (a, b)) =>
-                    if (a.size > 1 || b.size > 1) {
-                        throw new SparkException("There are multiple MatrixBlocks with indices: " +
-                            s"($blockRowIndex, $blockColIndex). Please remove them.")
-                    }
+  /*    def elementAdd(eps : Double): BlockMatrix = {        
+                
+            val addedBlocks = blocks.map { case ((blockRowIndex, blockColIndex), (a)) =>
+                    
                     if (a.isEmpty) {
-                        new MatrixBlock((blockRowIndex, blockColIndex), b.head)
-                    } else if (b.isEmpty) {
-                        new MatrixBlock((blockRowIndex, blockColIndex), a.head)
-                    } else {
-                        val result = a.head.toBreeze :+= eps
+                      zero=DenseMatrix.zeros[Double](rowsPerBlock, colsPerBlock)
+                      val result = zero:+= eps    
                         new MatrixBlock((blockRowIndex, blockColIndex), Matrices.fromBreeze(result))
+                    }  else {
+                      val result = a.toBreeze :+= eps    
+                      new MatrixBlock((blockRowIndex, blockColIndex), Matrices.fromBreeze(result))            
                     }
                 }
+            
                 new BlockMatrix(addedBlocks, rowsPerBlock, colsPerBlock, numRows(), numCols())
-            } else {
-                throw new SparkException("Cannot add matrices with different block dimensions")
-            }
-    }
+            
+    }*/
  
-    def elementMultiply(m1:BlockMatrix,m2: BlockMatrix): BlockMatrix = {
-        require(m1.numRows() == m2.numRows(), "Both matrices must have the same number of rows. " +
+    def elementMultiply(m2: BlockMatrix): BlockMatrix = {
+        require(numRows() == m2.numRows(), "Both matrices must have the same number of rows. " +
             s"A.numRows: ${numRows()}, B.numRows: ${m2.numRows()}")
-        require(m1.numCols() == m2.numCols(), "Both matrices must have the same number of columns. " +
+        require(numCols() == m2.numCols(), "Both matrices must have the same number of columns. " +
             s"A.numCols: ${m1.numCols()}, B.numCols: ${m2.numCols()}")
-        if (m1.rowsPerBlock == m2.rowsPerBlock && m1.colsPerBlock == m2.colsPerBlock) {
-            val addedBlocks = m1.blocks.cogroup(m2.blocks, m1.createPartitioner())
+        if (rowsPerBlock == m2.rowsPerBlock && colsPerBlock == m2.colsPerBlock) {
+            val addedBlocks = blocks.cogroup(m2.blocks, createPartitioner())
                 .map { case ((blockRowIndex, blockColIndex), (a, b)) =>
                     if (a.size > 1 || b.size > 1) {
                         throw new SparkException("There are multiple MatrixBlocks with indices: " +
@@ -406,13 +397,13 @@ class BlockMatrix @Since("1.3.0") (
             }
     }
     
-    def elementDivide(m1:BlockMatrix,m2: BlockMatrix): BlockMatrix = {
-        require(m1.numRows() == m2.numRows(), "Both matrices must have the same number of rows. " +
+    def elementDivide(m2: BlockMatrix): BlockMatrix = {
+        require(numRows() == m2.numRows(), "Both matrices must have the same number of rows. " +
             s"A.numRows: ${numRows()}, B.numRows: ${m2.numRows()}")
-        require(m1.numCols() == m2.numCols(), "Both matrices must have the same number of columns. " +
-            s"A.numCols: ${m1.numCols()}, B.numCols: ${m2.numCols()}")
-        if (m1.rowsPerBlock == m2.rowsPerBlock && m1.colsPerBlock == m2.colsPerBlock) {
-            val addedBlocks = m1.blocks.cogroup(m2.blocks, m1.createPartitioner())
+        require(numCols() == m2.numCols(), "Both matrices must have the same number of columns. " +
+            s"A.numCols: ${numCols()}, B.numCols: ${m2.numCols()}")
+        if (rowsPerBlock == m2.rowsPerBlock && colsPerBlock == m2.colsPerBlock) {
+            val addedBlocks = blocks.cogroup(m2.blocks, createPartitioner())
                 .map { case ((blockRowIndex, blockColIndex), (a, b)) =>
                     if (a.size > 1 || b.size > 1) {
                         throw new SparkException("There are multiple MatrixBlocks with indices: " +
